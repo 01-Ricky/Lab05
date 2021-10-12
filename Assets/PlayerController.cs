52cabf1226b59969abccf06857e8d5dee3728cc9
AliasNull <fatezg47@gmail.com>
2021-10-12 23:08:57 +0800

    Added Timer
    Collect all the coin before time run out

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,22 +11,41 @@ public class PlayerController : MonoBehaviour
     public Text cointext;
     public float coinscore;
 
+    public Text Timertext;
+    private float TimerValue;
+    public float timeleft;
+
+    public int timeRemaining;
+
     // Start is called before the first frame update
     void Start()
     {
         coinscore = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        timeleft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+        Timertext.text = "Timer: " + timeRemaining.ToString();
         cointext.text = "Coin: " + coinscore;
 
-        if (coinscore>=70)
+
+        if (coinscore == 70)
+        {          
+                GameObject.Find("FPSController").GetComponent<FirstPersonController>().m_MouseLook.m_cursorIsLocked = false;
+                SceneManager.LoadScene("GameWinScene");     
+
+        }
+       if (timeleft <= 0)
         {
             GameObject.Find("FPSController").GetComponent<FirstPersonController>().m_MouseLook.m_cursorIsLocked = false;
-            SceneManager.LoadScene("GameWinScene");
+            SceneManager.LoadScene("GameLoseScene");
+
         }
+
     }
 
     private void OnTriggerEnter(Collider other)
